9478f9fd02673155e9e636814dbfff2ca3ca5c57
saif-arshad <saifurrehman.web@gmail.com>
2025-03-30 21:16:20 +0500

    fix issue

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(
   '/downloads',
   (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Referrer-Policy', 'no-referrer'); // Added to override strict-origin-when-cross-origin
     next();
   },
   express.static(downloadsDir)
